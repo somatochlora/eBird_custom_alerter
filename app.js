f16054999f3386a50c544ec45c10e4c510135c5d
Reuven Martin <jaeger33@gmail.com>
2022-08-31 14:41:37 -0400

    functional checking for new checklists

--- app.js
+++ app.js
@@ -1,27 +1,65 @@
-const http = require('http');
 const dotenv = require('dotenv').config();
+const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
 const eBirdKey = process.env.EBIRD_API_KEY;
 
-const server = http.createServer((req, res) => {
-    let iNatresults = fetch("https://api.ebird.org/v2/product/lists/L7647094?key=" + eBirdKey);
-    iNatresults.then((response) => {
-        let data = response.json();
-        data.then((results) => {
-            console.log(results);
+async function fetchEbirdResults (location) {
+    let results = await fetch("https://api.ebird.org/v2/product/lists/" + location + "?key=" + eBirdKey + "&maxResults=200");
+    return await results.json();
+}
+
+let config = JSON.parse(fs.readFileSync('config.json'));
+let lastResults = JSON.parse(fs.readFileSync('savedResults.json'));
+
+let locations = config.locations;
+let savedChecklists = {}
+let toEmail = [];
+let promisesArray = [];
+
+for (let location of locations) {
+    let curLocationToSave = {
+        checklists:[]
+    }
+    savedChecklists[location.id] = curLocationToSave;
+
+    let eBirdResults = fetchEbirdResults(location.id)
+    promisesArray.push(eBirdResults);
+    eBirdResults.then(results => {
+        results.forEach(checklist => {
+           curLocationToSave.checklists.push(checklist.subId); 
         });
-    })
-    .catch((err) => {
-        console.log("unable to fetch");
-    })
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+
+        if (lastResults.hasOwnProperty(location.id)) {
+            results = results.filter(checklist => {
+                lastResults[location.id].checklists.indexOf(checklist.subId);
+            });
+        }
+
+        results.forEach(checklist => {
+            toEmail.push(checklist);
+        });
+    });
+}
+
+Promise.all(promisesArray).then(() => {
+    toEmail.forEach(checklist => {
+        let output = 
+            "Location: " +
+            checklist.loc.name +        
+            " Observer: " +
+            checklist.userDisplayName +
+            ", " +
+            checklist.numSpecies +
+            " species observed, " +
+            checklist.obsDt +
+            " " +
+            checklist.obsTime;
+        console.log(output);
+    });
+    fs.writeFileSync('savedResults.json', JSON.stringify(savedChecklists, null, 2));
+
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-    console.log(eBirdKey);
-});
+
+
+
